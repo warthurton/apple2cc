fdbad016136cc16f9fd45e15d89a60dafe6f7bb4
Wayne A. Arthurton <warthurton@arthurton.com>
2015-07-23 15:46:06 -0400

    Initial Commit
    Cleaned up a2sudoku to create a generic package.
    
    Todo:
    - Add in more Copyright details and links

---
+++ apple2cc/main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ * a2cc project
+ * Scaffolding for a cc65 apple 2 project
+ *
+ * Created by Wayne Arthurton on 2015-07-23
+ *
+ * Derived from:
+ * Created by Jeremy Rand on 2015-07-15.
+ * Copyright (c) 2015 Jeremy Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+
+    // Code here
+    
+    return 0;
+}
